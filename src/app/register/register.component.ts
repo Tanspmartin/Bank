53d9deee93f0baf31dced58977ae4a77947d1d38
tanspmartin <tanspmartin@gmail.com>
2022-10-07 09:49:44 +0530

    form validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -12,15 +13,19 @@ export class RegisterComponent implements OnInit {
   accono=""
   passwrd=""
 
-  constructor(private ds:DataService,private router:Router) { }
+  registerForm=this.fb.group({
+    uname:['',[Validators.required,Validators.pattern('[a-zA-Z]+')]],accono:['',[]],passwrd:['',[]]
+  })
+
+  constructor(private ds:DataService,private router:Router,private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
   register()
   {
-    var uname=this.uname
-    var accono=this.accono
-    var passwrd=this.passwrd
+    var uname=this.registerForm.value.uname
+    var accono=this.registerForm.value.accono
+    var passwrd=this.registerForm.value.passwrd
     const result=this.ds.register(accono,uname,passwrd)
     if(result)
     {
